f3dd64927a92c9a71370911b213c3096089a9d8e
braverokmc79 <braverokmc79@gmail.com>
2024-01-06 19:55:12 +0900

    04.승자 결정하기

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,11 +6,49 @@ import "./Board.css"
 export default function Board(){
     const [squares, setSquares]=useState(Array(9).fill(null));
     const [xIsNext, setXIsNext] = useState(true);
-    const status=`Next Player : ${xIsNext ? 'X' : 'O'}`;
+    //const status=`Next Player : ${xIsNext ? 'X' : 'O'}`;
+
+
+    const calculateWinner=(squares)=>{
+        const lines=[
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]            
+        ]
+        for(let i=0; i<lines.length; i++){
+            const [a,b,c]=lines[i];
+            if(squares[a]&&squares[a]===squares[b]&&squares[a]===squares[c]){
+                return squares[a];
+            }
+        }
+        
+        return null;
+    }
+
+
+    const winner =calculateWinner(squares);
+    let status;
+    if(winner){
+        status=`Winner is ${winner}`;
+    }else{
+        status=`Next Player : ${xIsNext? 'X' : 'O'}`;
+    }
+
+
 
     const handleClick=(no)=>{ 
         //slice 깊은 복사
         const newSquares=squares.slice();
+
+        if(calculateWinner(newSquares) || newSquares[no]){
+             return;
+        }
+
         newSquares[no]=xIsNext ? 'X' :'O';
         setSquares(newSquares);
         setXIsNext(prev=>!prev);
